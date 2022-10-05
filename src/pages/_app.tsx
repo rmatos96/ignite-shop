45d6d543027ef235ab56a21b9d5df4577ff90b7b
unknown <r.matos96@gmail.com>
2022-10-05 11:54:29 -0300

    adding features

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+
 import logoImg from "../assets/logo.svg"
 import { Container, Header } from "../styles/pages/app"
+
+import Image from 'next/future/image'
 
 globalStyles()
 
@@ -9,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       
       <Component {...pageProps} />
