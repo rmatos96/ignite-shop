0ea2d7c319e25e5c353b93172bc9fca809569ce0
unknown <r.matos96@gmail.com>
2022-10-10 12:07:49 -0300

    adding features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/future/image"
+import Link from "next/link"
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import { useKeenSlider } from "keen-slider/react"
@@ -14,7 +15,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -31,14 +32,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} alt='' width={520} height={480} />
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} alt='' width={520} height={480} />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
+import { ImageContainer } from '../styles/pages/success'
+import { SuccessContaienr } from '../styles/pages/success'
 
 export default function Success() {
   return (
-    <h1>Sucesso</h1>
+    <SuccessContaienr>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <p>
+        Uhull <strong>Rodrigo Matos</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href='/'>
+        Voltar ao catálogo
+      </Link>
+    </SuccessContaienr>
   )
 }
